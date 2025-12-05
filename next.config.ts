753305f29666e2e0e65bcb29eeb4898bbef0fc75
Dexter Du <wxdu64@gmail.com>
2025-12-06 07:51:48 +0800

    chore: adjust build config and image settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ykietuljwfgtxwgfqhiv.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+    qualities: [25, 50, 75, 85, 100],
+  },
+  // output: "export",
 };
 
 export default nextConfig;
